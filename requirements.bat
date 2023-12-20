9c8e0383beb02259522f2616045bff6d8f384603
Pixel2175 <147437230+Pixel2175@users.noreply.github.com>
2023-12-20 06:12:41 -0800

    Update and rename requirements.bat to requirements.txt

--- requirements.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-python -m pip install requests PyQt5 tqdm
-echo msgbox "The packages have been downloaded." > %temp%\tempmessage.vbs
-call %temp%\tempmessage.vbs
-del %temp%\tempmessage.vbs
-pause
